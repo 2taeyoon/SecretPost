10bed04307d5187d82a3427bd626b0f1a74ed38e
2taeyoon <2tae@kakao.com>
2023-09-08 18:28:04 +0900

    dsada

--- src/components/Login/SecretLogo.tsx
+++ src/components/Login/SecretLogo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SecretLogo = () => {
     return (
         <div className='secret_logo'>
-            <div className='secret_post_image' style={{ backgroundImage: 'url(./image/secret_post_image_logo.png)' }}></div>
+            <div className='secret_post_image'></div>
             <div className='secret_post_text' style={{ backgroundImage: 'url(./image/secret_post_text_logo.png)' }}></div>
         </div>
     )
